b3dccc34de3f3d33da11dc36193636f4ffda159a
dirstart <835041058@qq.com>
2018-05-30 12:27:19 +0800

    移动端阅读器update

--- src/Mobile/pages/Reader/index.js
+++ src/Mobile/pages/Reader/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import {Layout, Icon} from 'antd';
+import {Layout, Icon, message} from 'antd';
+import Axios from 'axios';
 
 import './index.less';
 
@@ -20,32 +21,88 @@ class ReaderMain extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            bookList: []
-         }
+            bookList: [],
+            isReading: false,
+            bookMsg: {},
+            isShowNav: false
+        }
     }
+
+    componentDidMount() {
+        const search = window.location.search.split('?')[1];
+        const ID = ((search && search.length) || false) ? search.split('=')[1] : 'no';
+        if (ID !== 'no') {
+            this.setState({
+                isReading: true
+            });
+            this.initStatus(ID);
+        }
+    }
+
     render() { 
+        const {isReading, bookMsg, isShowNav} = this.state;
         return ( <div className="mobile-reader-wrap">
-            <Layout>
-                <Header style={headerStyle}>
-                    <span>Reading</span>
-                    <Link to="/m_search" className="mobile-reader-search-icon-wrap">
-                        <Icon type="search" className="mobile-reader-search-icon" />
-                    </Link>
-                </Header>
-                <Content style={contentStyle}>
-                    {
-                        this.state.bookList.length === 0 ?
-                        (
-                            <div className="mobile-reader-booklist-null">
-                                书架空空的！快去添加点书吧！
-                            </div>
-                        )
-                        :
-                        <div>有书了</div>
-                    }
-                </Content>
-            </Layout>
+            {
+                isReading ?
+                (<div className="mobile-read-mod-wrap">
+                    <div className="mobile-read-touch-panel"
+                        onClick={this.handleTouchPanel.bind(this)}
+                    ></div>
+                    <div className="mobile-read-mod-top"
+                        style={{display: isShowNav ? 'block' : 'none'}}
+                    >2</div>
+                    <div className="mobile-read-mod-content">
+                        {bookMsg && bookMsg.content}
+                    </div>
+                    <div className="mobile-read-mod-bottom"
+                        style={{display: isShowNav ? 'block' : 'none'}}
+                    >1</div>
+                </div>)
+                :
+                (<Layout>
+                    <Header style={headerStyle}>
+                        <span>Reading</span>
+                        <Link to="/m_search" className="mobile-reader-search-icon-wrap">
+                            <Icon type="search" className="mobile-reader-search-icon" />
+                        </Link>
+                    </Header>
+                    <Content style={contentStyle}>
+                        {
+                            this.state.bookList.length === 0 ?
+                            (
+                                <div className="mobile-reader-booklist-null">
+                                    书架空空的！快去添加点书吧！
+                                </div>
+                            )
+                            :
+                            <div>有书了</div>
+                        }
+                    </Content>
+                </Layout>)
+            }
+            
         </div> )
+    }
+
+    async initStatus(ID) {
+        const {data} = await Axios.get('/pc/library/by-id', {
+            params: {
+                ID: ID
+            }
+        });
+        if (data.success) {
+            this.setState({
+                bookMsg: data.data
+            });
+        } else {
+            message.error('数据库获取失败');
+        }
+    }
+
+    handleTouchPanel() {
+        this.setState({
+            isShowNav: !this.state.isShowNav
+        })
     }
 }
  
